dfecd5ca80601188eb9eaf541b7bcfcb8617bb9d
Harold <m194020@usna.edu>
2017-12-14 10:42:53 -0800

    d

--- dashboard.php
+++ dashboard.php
@@ -136,7 +136,12 @@
   <div class="content-wrapper">
     <div class="container-fluid">
       <!-- Breadcrumbs-->
-      
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="dashboard.php">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">Welcome</li>
+      </ol>
       <div class="row">
         <div class="col-12">
 
